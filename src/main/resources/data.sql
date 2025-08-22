c66831aaecdd8c2cc4ef7ab0a203ac1cec6dd82a
evenhu9 <evenhu9@gmail.com>
2025-08-22 14:03:02 +0800

    update

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,12 @@
 -- This script initializes data for the application.
 -- Passwords are encrypted using BCrypt.
--- 'admin' -> $2a$10$v7g.DqP6X.fKjOq8sB2d5e/c.5o9hJ2qJ9qY8iM.X.p8sQ.zG5qBq
--- 'user' -> $2a$10$tP6A4B7G.6p.8w.i5qE9c.FzJ9nL2nU2f.b4sK7vY.p2sT.zG3wAa
+-- 'hyw_security@' -> $2a$10$rK8CubYH0Pep.BPEmnO32uUvlsZ1Aat3Ibnoj6LwryyUqSvtzDpD.
+-- 'hyw_security*' -> $2a$10$l1ZDQFViYF.eGjqXqhg.Xun2N.lKexvPYK8NmwXHXFqqBKa8k5.tO
+-- 'hyw_security#' -> $2a$10$aPzEXo799B7L4ZhjJ.YJqup4EjJXFJsbsLB3si.vZz5TyZ73VALp2
 
-INSERT INTO users (username, password, enabled) VALUES ('admin', '$2a$10$v7g.DqP6X.fKjOq8sB2d5e/c.5o9hJ2qJ9qY8iM.X.p8sQ.zG5qBq', true);
-INSERT INTO users (username, password, enabled) VALUES ('user', '$2a$10$tP6A4B7G.6p.8w.i5qE9c.FzJ9nL2nU2f.b4sK7vY.p2sT.zG3wAa', true);
-INSERT INTO users (username, password, enabled) VALUES ('disabled_user', '$2a$10$tP6A4B7G.6p.8w.i5qE9c.FzJ9nL2nU2f.b4sK7vY.p2sT.zG3wAa', false);
+INSERT INTO users (username, password, enabled) VALUES ('admin', '$2a$10$rK8CubYH0Pep.BPEmnO32uUvlsZ1Aat3Ibnoj6LwryyUqSvtzDpD.', true);
+INSERT INTO users (username, password, enabled) VALUES ('user', '$2a$10$l1ZDQFViYF.eGjqXqhg.Xun2N.lKexvPYK8NmwXHXFqqBKa8k5.tO', true);
+INSERT INTO users (username, password, enabled) VALUES ('disabled_user', '$2a$10$aPzEXo799B7L4ZhjJ.YJqup4EjJXFJsbsLB3si.vZz5TyZ73VALp2', false);
 
 INSERT INTO user_roles (user_id, role) VALUES (1, 'ROLE_ADMIN');
 INSERT INTO user_roles (user_id, role) VALUES (1, 'ROLE_USER'); -- admin is also a user
